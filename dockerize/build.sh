87e8a85d087fc24d307c3cc4c303f9b81cdb4d26
Stefan <dev@failing.systems>
2026-02-02 01:11:09 +0100

    Install latest pi-coding-agent & build release image
    Update Dockerfile.release to install @mariozechner/pi-coding-agent@latest so the image uses the most recent published package. Adjust dockerize/build.sh to build with Dockerfile.release by default and comment out the Dockerfile.git build command.

--- dockerize/build.sh
+++ dockerize/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#docker build -t pi-coding-agent -f Dockerfile.release .
-docker build -t pi-coding-agent -f Dockerfile.git .
+docker build -t pi-coding-agent -f Dockerfile.release .
+#docker build -t pi-coding-agent -f Dockerfile.git .
